61af9ea8b8cbd9b16d4adf9bb0a07c953da6ea65
DenMitter <57758gfk@gmail.com>
2024-09-18 18:23:59 +0300

    delete navbar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -26,23 +26,7 @@
     </style>
 </head>
 <body>
-    <div class="container">
-        <nav class="navbar navbar-expand-lg navbar-light bg-light">
-            <div class="container-fluid">
-              <a class="navbar-brand" href="#">TEST API</a>
-              <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-              </button>
-              <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                  <li class="nav-item"><a class="nav-link active" aria-current="page" href="#">Главная</a></li>
-                  <li class="nav-item"><a class="nav-link" href="#">Мероприятия и события</a></li>
-                  <li class="nav-item"><a class="nav-link" href="#">Забронировать</a></li>
-                </ul>
-              </div>
-            </div>
-        </nav>
-        
+    <div class="container">        
         <main class="py-4">
             @yield('content')
         </main>
